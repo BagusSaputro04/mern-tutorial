65170bfae049a2d429af693918c4c5009e12ca24
BagusSaputro04 <ajegbimbim03@gmail.com>
2022-07-05 19:50:51 +0700

    membuat file route dan memanggil route

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-const express = require('express')
+const express = require("express");
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+
 
 const app = express()
 
 app.get('/api/goals',(req,res)=>{
-    res.json({message: 'Get Goals'})
+    res.status(200).json({message: 'Get Goals'})
 })
-app.listen(port,()=> console.log('server started on port ${port}'))
+app.use('/api/goals',require('./routes/goalRoutes.js'))
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
