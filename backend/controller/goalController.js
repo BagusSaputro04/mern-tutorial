22b44cc94ba1fed89fafdc0795498e990be3df6a
BagusSaputro04 <ajegbimbim03@gmail.com>
2022-07-05 21:09:43 +0700

    connecting controller fungction

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -4,8 +4,22 @@ const getGoals = (req,res) => {
 const {getGoals} = require("../controllers/goalController")
 const router = require("../routes/goalRoutes")
 
+const setGoal = (req,res) => {
+    res.status(200).json({message: 'Set Goals controller'})
+}
+
+const updateGoal = (req,res) => {
+    res.status(200).json({message: `Update Goal controller ${req.params.id}`})
+}
+
+const deleteGoal = (req,res) => {
+    res.status(200).json({message: `Delete Goal controller ${req.params.id}`})
+}
 module.exports = {
-    getGoals
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
 }
 
 router.get("/".getGoals)
