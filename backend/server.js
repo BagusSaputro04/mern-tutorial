34c9e97ae4327895fa1e3575342bccc516aa9b19
BagusSaputro04 <ajegbimbim03@gmail.com>
2022-07-11 10:25:10 +0700

    menambah block error

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
-const express = require("express");
+const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 const app = express()
 app.use(express.json())
@@ -7,10 +8,9 @@ app.use(express.urlencoded({ extended: false}))
 
 app.use('/api/goals',require('./routes/goalRoutes.js'))
 
-
 app.get('/api/goals',(req,res)=>{
     res.status(200).json({message: 'Get Goals'})
 })
 app.use('/api/goals',require('./routes/goalRoutes.js'))
-
+app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
